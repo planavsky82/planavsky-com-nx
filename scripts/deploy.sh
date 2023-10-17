abd85e953c9db1de94e62d97d3274952bc42ee05
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP-2.home>
2023-10-17 12:56:44 -0400

    firebase

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+nx build angular-app
+
+# cd angular
+# npm run e2e:mffr:clean
+# npm run build:mffr
+# cd ..
+# cd blog
+# npm run build
+# cd ..
+# rm -rf dist
+# mkdir -p dist
+# cd angular
+# mv dist/mffr ../dist
+# cd ../dist
+# mkdir -p web-components
+# cd ../stencil-web-components
+# npm run build
+# mv dist ../dist/web-components
+# cd ../dist/mffr
+# mkdir -p web-components
+# cd ../../stencil-web-components
+# npm run build
+# mv dist ../dist/mffr/web-components
+# cd ../dist
+# mkdir -p blog
+# cd ../blog
+# mv public ../dist/blog
+# firebase deploy
