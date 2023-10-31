938f6bb90606a3168b8926e208cc41a861760306
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP-2.home>
2023-10-30 23:03:35 -0400

    deploy planit docs

--- scripts/deploy-planit.sh
+++ scripts/deploy-planit.sh
@@ -8,10 +8,23 @@ npm run test:e2e
 npm run test:planit
 
 # deploy docs site
+npm run build:planit-docs
+cd dist/apps/planit-docs/www
+rm -rf .git
+git init
+git remote add origin git@github.com:planavsky82/component-lib-platform-intro.git
+git fetch --all
+git branch -d gh-pages
+git add -A
+git commit -m 'update gh-pages'
+git checkout -b gh-pages
+git push --set-upstream origin gh-pages -f
 
 # deploy storybook via Firebase
 
 # copy react and angular wrappers to planit dist
+
+# deploy npm version of library
 
 # deploy server hosted planit library
 
