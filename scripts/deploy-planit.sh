fe841a50bde6590f88957af329395ea8199e806a
Anthony Planavsky <anthonyplanavsky@Anthonys-MacBook-Pro-2.local>
2023-11-13 20:01:15 -0500

    chore(release): create temporary package.json

--- scripts/deploy-planit.sh
+++ scripts/deploy-planit.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env sh
 
 # run e2e and a11y tests
-npx playwright install
-npm run test:e2e
+# npx playwright install
+# npm run test:e2e
 
-# run unit tests
-npm run test:planit
+# # run unit tests
+# npm run test:planit
 
-# deploy docs site
-npm run build:planit-docs
-cd dist/apps/planit-docs/www
-rm -rf .git
-git init
-git remote add origin git@github.com:planavsky82/component-lib-platform-intro.git
-git fetch --all
-git branch -d gh-pages
-git add -A
-git commit -m 'update gh-pages'
-git checkout -b gh-pages
-git push --set-upstream origin gh-pages -f
-cd ../../../../
+# # deploy docs site
+# npm run build:planit-docs
+# cd dist/apps/planit-docs/www
+# rm -rf .git
+# git init
+# git remote add origin git@github.com:planavsky82/component-lib-platform-intro.git
+# git fetch --all
+# git branch -d gh-pages
+# git add -A
+# git commit -m 'update gh-pages'
+# git checkout -b gh-pages
+# git push --set-upstream origin gh-pages -f
+# cd ../../../../
 
-# build planit lib
-npm run build:planit:prod
+# # build planit lib
+# npm run build:planit:prod
 
-# build planit storybook
-npm run build:planit-storybook
+# # build planit storybook
+# npm run build:planit-storybook
 
-# deploy storybook via Firebase
-firebase deploy --only hosting:planit-web-components
+# # deploy storybook via Firebase
+# firebase deploy --only hosting:planit-web-components
 
 # deploy npm version of library
 cd dist/libs/planit
@@ -44,21 +44,21 @@ npm version $1
 cp package.json ../../../libs/planit/
 cp ../../../libs/planit/README.md README.md
 
-echo 'Publishing NPM Package ...'
-npm publish --access=public
+# echo 'Publishing NPM Package ...'
+# npm publish --access=public
 
-echo 'Adding GIT Commit ...'
-rm -rf package-tmp.json
-git add -A
-git commit -m "chore(release): $1"
-git push
-cd ../../../
+# echo 'Adding GIT Commit ...'
+# rm -rf package-tmp.json
+# git add -A
+# git commit -m "chore(release): $1"
+# git push
+# cd ../../../
 
-echo 'Deployment Complete.'
+# echo 'Deployment Complete.'
 
-# deploy server hosted planit library
-firebase deploy --only hosting:planit-lib
+# # deploy server hosted planit library
+# firebase deploy --only hosting:planit-lib
 
-# deploy React demo
-npm run build:react-sample-app
-firebase deploy --only hosting:planit-react-sample-app
+# # deploy React demo
+# npm run build:react-sample-app
+# firebase deploy --only hosting:planit-react-sample-app
