862eacf1e26d2ab60c2aebfb9545a3a4ef0120be
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP-2.home>
2024-12-01 12:52:25 -0500

    develop fantasy football rankings demo

--- apps/products/collection/demo-data/football.js
+++ apps/products/collection/demo-data/football.js
@@ -21,3 +21,52 @@ let players = [
     "colors": [ "#32a852", "#a83238", "#a83238" ]
   }
 ];
+
+fetch('https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
+    'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
+  }
+}) // Replace with your API URL
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Assuming the API returns JSON data
+  })
+  .then(data => {
+    // Process the received data
+    let teams = data;
+    teams.forEach((team) => {
+      console.log(team);
+      /* fetch('https://nfl-api-data.p.rapidapi.com/nfl-player-listing/v1/data?id=22', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
+          'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
+        }
+      }) // Replace with your API URL
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json(); // Assuming the API returns JSON data
+        })
+        .then(data => {
+          // Process the received data
+          let teams = data;
+          console.log(teams);
+        })
+        .catch(error => {
+          // Handle errors
+          console.error('Error fetching data:', error);
+        }); */
+    });
+  })
+  .catch(error => {
+    // Handle errors
+    console.error('Error fetching data:', error);
+  });
