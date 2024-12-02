57129b9a330f012e631380a267e23ee751d7f305
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP-2.home>
2024-12-02 07:47:58 -0500

    develop fantasy football rankings demo

--- apps/products/collection/demo-data/football.js
+++ apps/products/collection/demo-data/football.js
@@ -22,54 +22,60 @@ let players = [
   }
 ];
 
-fetch('https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data', {
-  cache: 'force-cache',
-  method: 'GET',
-  headers: {
-    'Cache-Control': 'max-age=86400',
-    'Content-Type': 'application/json',
-    'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
-    'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
-  }
-}) // Replace with your API URL
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+if (localStorage.hasOwnProperty('players')) {
+  console.log('local storage set');
+} else {
+  fetch('https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data', {
+    cache: 'force-cache',
+    method: 'GET',
+    headers: {
+      'Cache-Control': 'max-age=86400',
+      'Content-Type': 'application/json',
+      'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
+      'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
     }
-    return response.json(); // Assuming the API returns JSON data
   })
-  .then(data => {
-    // Process the received data
-    let teams = data;
-    console.log(teams);
-    //teams.forEach((team) => {
-    //  console.log(team);
-      /* fetch('https://nfl-api-data.p.rapidapi.com/nfl-player-listing/v1/data?id=22', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
-          'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
-        }
-      }) // Replace with your API URL
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Assuming the API returns JSON data
+    })
+    .then(data => {
+      // Process the received data
+      let teams = data;
+      teams.forEach((teamObj) => {
+        fetch('https://nfl-api-data.p.rapidapi.com/nfl-player-listing/v1/data?id=' + teamObj.team.id, {
+          cache: 'force-cache',
+          method: 'GET',
+          headers: {
+            'Cache-Control': 'max-age=86400',
+            'Content-Type': 'application/json',
+            'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
+            'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
           }
-          return response.json(); // Assuming the API returns JSON data
         })
-        .then(data => {
-          // Process the received data
-          let teams = data;
-          console.log(teams);
-        })
-        .catch(error => {
-          // Handle errors
-          console.error('Error fetching data:', error);
-        }); */
-    //});
-  })
-  .catch(error => {
-    // Handle errors
-    console.error('Error fetching data:', error);
-  });
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            console.log(teamObj.team.displayName);
+            let playerData = data;
+            console.log(playerData.athletes.find((athlete) => {
+              return athlete.position === 'offense';
+            }).items);
+          })
+          .catch(error => {
+            console.error('Error fetching data:', error);
+          });
+      });
+      localStorage.setItem('players', {});
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('Error fetching data:', error);
+    });
+}
