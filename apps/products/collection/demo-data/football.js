eca3ba0a86964a196bb8edc081f7b626de29ff4e
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP-2.home>
2024-12-10 00:59:27 -0500

    develop fantasy football rankings demo

--- apps/products/collection/demo-data/football.js
+++ apps/products/collection/demo-data/football.js
@@ -119,102 +119,18 @@ async function loadAPIlData() {
         let playerList = [];
         let teamList = [];
 
-        //teams.forEach((teamObj) => {
-        let teamObj = teams[0];
-        return fetch('https://nfl-api-data.p.rapidapi.com/nfl-player-listing/v1/data?id=1', {
-            cache: 'force-cache',
-            method: 'GET',
-            headers: {
-              'Cache-Control': 'max-age=86400',
-              'Content-Type': 'application/json',
-              'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
-              'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
-            }
-          })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-          .then(data => {
-            teamList.push(teamObj.team);
-            let playerData = data;
-            let players = playerData.athletes.find((athlete) => {
-              return athlete.position === 'offense';
-            }).items;
-            players.forEach((player) => {
-              playerList.push({
-                headshot: player.headshot,
-                id: player.id,
-                uid: player.uid,
-                guid: player.guid,
-                shortName: player.shortName,
-                slug: player.slug,
-                status: player.status,
-                number: player.jersey,
-                position: player.position,
-                firstName: player.firstName,
-                lastName: player.lastName,
-                experience: player.experience,
-                college: player.college,
-                displayName: player.displayName,
-                injuries: player.injuries,
-                team: teamObj.team
-              });
-            });
-            localStorage.setItem('mffrPlayerData', JSON.stringify(playerList));
-            localStorage.setItem('mffrTeamData', JSON.stringify(teamList));
-            return loadLocalData();
-          })
-          .catch(error => {
-            console.error('Error fetching data:', error);
-          });
-        //});
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('Error fetching data:', error);
-      });
-  } else {
-    return loadLocalData();
-  }
-}
-
-async function loadAPIlData2() {
-  if (!localStorage.hasOwnProperty('mffrPlayerData')) {
-    fetch('https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data', {
-      cache: 'force-cache',
-      method: 'GET',
-      headers: {
-        'Cache-Control': 'max-age=86400',
-        'Content-Type': 'application/json',
-        'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
-        'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); // Assuming the API returns JSON data
-      })
-      .then(data => {
-        // Process the received data
-        let teams = data;
-        let playerList = [];
-        let teamList = [];
-        teams.forEach((teamObj) => {
-          fetch('https://nfl-api-data.p.rapidapi.com/nfl-player-listing/v1/data?id=' + teamObj.team.id, {
-            cache: 'force-cache',
-            method: 'GET',
-            headers: {
-              'Cache-Control': 'max-age=86400',
-              'Content-Type': 'application/json',
-              'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
-              'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
-            }
-          })
+        async function fetchData(urls, teams) {
+          const promises = urls.map(async (url, index) => {
+            const response = await fetch(url, {
+              cache: 'force-cache',
+              method: 'GET',
+              headers: {
+                'Cache-Control': 'max-age=86400',
+                'Content-Type': 'application/json',
+                'X-Rapidapi-Host': 'nfl-api-data.p.rapidapi.com',
+                'X-Rapidapi-Key': 'cf3cb436dcmsh713196812fbd533p195348jsnf6ac112ec393'
+              }
+            })
             .then(response => {
               if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -222,7 +138,7 @@ async function loadAPIlData2() {
               return response.json();
             })
             .then(data => {
-              teamList.push(teamObj.team);
+              let teamObj = teams[index];
               let playerData = data;
               let players = playerData.athletes.find((athlete) => {
                 return athlete.position === 'offense';
@@ -247,17 +163,33 @@ async function loadAPIlData2() {
                   team: teamObj.team
                 });
               });
-              localStorage.setItem('mffrPlayerData', JSON.stringify(playerList));
-              localStorage.setItem('mffrTeamData', JSON.stringify(teamList));
-              /* if (localStorage.getItem('mffrUserRankings') === undefined) {
-                console.log('undefined');
-              } */
-              return loadLocalData();
+              return playerList;
             })
             .catch(error => {
               console.error('Error fetching data:', error);
             });
+            return response;
+          });
+
+          const results = await Promise.all(promises);
+          return results;
+        }
+
+        let urls = [];
+        teams.forEach((teamObj) => {
+          urls.push('https://nfl-api-data.p.rapidapi.com/nfl-player-listing/v1/data?id=' + teamObj.team.id);
+          teamList.push(teamObj.team);
         });
+
+        return fetchData(urls, teams)
+          .then(data => {
+            localStorage.setItem('mffrPlayerData', JSON.stringify(data[0]));
+            localStorage.setItem('mffrTeamData', JSON.stringify(teamList));
+            return loadLocalData();
+          })
+          .catch((error) => {
+            return error;
+          });
       })
       .catch(error => {
         // Handle errors
