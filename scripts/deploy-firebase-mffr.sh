41f7052f91cf20e94a58ec36048a161345b6dc7f
Anthony Planavsky <anthonyplanavsky@Anthonys-MacBook-Pro-2.local>
2025-03-30 15:54:33 -0500

    integrate API into base app

---
+++ scripts/deploy-firebase-mffr.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+cd functions && npm ci && cd ..
+firebase deploy --only functions
+firebase deploy --only hosting:myfantasyfootballrankings
+
+echo 'THE config.js FILE MUST EXIST IN THE FUNCTIONS FOLDER!!!!'
+
+# cd angular
+# npm run e2e:mffr:clean
+# npm run build:mffr
+# cd ..
+# cd blog
+# npm run build
+# cd ..
+# rm -rf dist
+# mkdir -p dist
+# cd angular
+# mv dist/mffr ../dist
+# cd ../dist
+# mkdir -p web-components
+# cd ../stencil-web-components
+# npm run build
+# mv dist ../dist/web-components
+# cd ../dist/mffr
+# mkdir -p web-components
+# cd ../../stencil-web-components
+# npm run build
+# mv dist ../dist/mffr/web-components
+# cd ../dist
+# mkdir -p blog
+# cd ../blog
+# mv public ../dist/blog
+# firebase deploy
